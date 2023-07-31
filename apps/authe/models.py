66c82331a9bfc1aaecc049491f9c61cd97ce5825
dasifue <dasifue@gmail.com>
2023-08-01 01:06:44 +0600

    created custom user model

--- apps/authe/models.py
+++ apps/authe/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    image = models.ImageField("Avatar", upload_to="media/uploads/avatars/", default="media/default/avatar.png")
+    phone = models.CharField("Phone", max_length=20)    
+    date_updated = models.DateTimeField("Date updated", auto_now=True)
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    def __str__(self):
+        return self.username
+
+    @property
+    def full_name(self):
+        full_name = f"{self.first_name} {self.last_name}"
+        return full_name.strip()
+    
+    
