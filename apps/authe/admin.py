6e422fef5abfa246718ba6d73c130c03e57bc496
dasifue <dasifue@gmail.com>
2023-08-01 02:01:34 +0600

    user registered on admin site

--- apps/authe/admin.py
+++ apps/authe/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+class AdminSiteUser(admin.ModelAdmin):
+    list_display = ["username", "full_name", "email"]
+    list_filter = ["is_staff", "is_superuser", "is_active"]
+    search_fields = ["username", "phone", "email"]
+
+admin.site.register(User, AdminSiteUser)
