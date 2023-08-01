7c6b43679be7aa00117bb523d8e929c676fe8ba0
dasifue <dasifue@gmail.com>
2023-08-01 20:19:43 +0600

    added annotation

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 
+# Модель продукта.
+# Я не стал прописывать сложную модель, так как для ТЗ нет необходимости
+# Я хотел прописать ещё и категории одеждны, размеры, но решил модель оставить абстрактной
+
 class Product(models.Model):
-    image = models.ImageField("Image", upload_to="media/uploads/products/", default="media/default/product.jpg")
-    name = models.CharField("Name", max_length=255)
-    description = models.TextField("Description", null=True)
-    uploaded = models.DateTimeField("Uploaded", auto_now_add=True)
-    price = models.DecimalField("Price", decimal_places=2, max_digits=12, null=False)
-    is_available = models.BooleanField("Availability", default=True, null=False)
+    image = models.ImageField("Image", upload_to="media/uploads/products/", default="media/default/product.jpg") #Поле image - это картинка продукта. Загружаются фотографии в media, в случае, если создаётся продукт без картинки, то берётся дефолтная картинка  
+    name = models.CharField("Name", max_length=255) # Название продукта. Обычный заголовок на 255 элементов
+    description = models.TextField("Description", null=True) #Описание - это текст без лимита на количестов элементов
+    uploaded = models.DateTimeField("Uploaded", auto_now_add=True) #Дата добавления продукта в магазин
+    price = models.DecimalField("Price", decimal_places=2, max_digits=12, null=False) #Цена - decimal, где всего 12 цифр, из которых 2 стоят после точки
+    is_available = models.BooleanField("Availability", default=True, null=False) #Статус продукта. Доступность / наличие
 
     class Meta:
         verbose_name = "Product"
@@ -17,5 +21,9 @@ class Product(models.Model):
     
     @property
     def short_description(self):
-        description = self.description.strip()
+        """
+        Проперти метод преображает метод в атрибут.
+        Здесь метод выводит первые 40 элементов из описания
+        """
+        description = self.description.strip() #избавляемся от возможных пробелов слева и справа
         return description[:41]
--- apps/posts/serializers.py
+++ apps/posts/serializers.py
@@ -2,9 +2,14 @@ from rest_framework import serializers
 
 from .models import Product
 
-
+# Сериализаторы используются для преобразования python объектов в JSON формат и наоборот
+# Прописал всего 2 сериализатора: 1 - для работы со всеми продуктами, 2 - для добавления продукта в фавориты 
 
 class ProductSerializer(serializers.ModelSerializer):
+
+    """
+    Сериализатор выводит все поля продукта в формате JSON
+    """
 
     class Meta:
         model = Product
@@ -14,13 +19,18 @@ class ProductSerializer(serializers.ModelSerializer):
 class FavoritesCreateSerializer(serializers.Serializer):
     product_id = serializers.IntegerField()
 
-    def validate(self, data):
-        id_ = data.get("product_id")
-        try:
+    """
+    Сериализатор принимает в себя ID продукта, проверяет даные на валидность
+    """
+
+
+    def validate(self, data): #Метод validate вызывается самостоятельно в методе is_valid
+        id_ = data.get("product_id") #Определяем отправленный пользователем id продукта
+        try: #Проверям: существует ли продукт с введённым id
             Product.objects.get(pk=id_)
-        except Product.DoesNotExist:
+        except Product.DoesNotExist: #Если такого продукта не существует, то выводим ошибку
             raise serializers.ValidationError({"error": "Product does not exist"})
-        else:
+        else: #Иначе пропускаем данные
             return data
 
 
